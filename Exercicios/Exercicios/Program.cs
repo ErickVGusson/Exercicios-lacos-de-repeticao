5c466245b8bbcabb4e99a3efeea7bff7c3e7b6b0
ErickVGusson <92764323+ErickVGusson@users.noreply.github.com>
2023-09-12 09:33:56 -0300

    Completado

--- Exercicios/Exercicios/Program.cs
+++ Exercicios/Exercicios/Program.cs
@@ -10,6 +10,9 @@ namespace Exercicios
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine(" ______     __  __     ______     ______     ______     __     ______     __     ______     ______    \r\n/\\  ___\\   /\\_\\_\\_\\   /\\  ___\\   /\\  == \\   /\\  ___\\   /\\ \\   /\\  ___\\   /\\ \\   /\\  __ \\   /\\  ___\\   \r\n\\ \\  __\\   \\/_/\\_\\/_  \\ \\  __\\   \\ \\  __<   \\ \\ \\____  \\ \\ \\  \\ \\ \\____  \\ \\ \\  \\ \\ \\/\\ \\  \\ \\___  \\  \r\n \\ \\_____\\   /\\_\\/\\_\\  \\ \\_____\\  \\ \\_\\ \\_\\  \\ \\_____\\  \\ \\_\\  \\ \\_____\\  \\ \\_\\  \\ \\_____\\  \\/\\_____\\ \r\n  \\/_____/   \\/_/\\/_/   \\/_____/   \\/_/ /_/   \\/_____/   \\/_/   \\/_____/   \\/_/   \\/_____/   \\/_____/ \r\n                                                                                                      \n\n");
+            Console.ResetColor();
 
             Console.WriteLine("Exercício 1 - Números Pares em um intervalo");
             Console.WriteLine("Exercício 2 - Sequência de Fibonacci");
@@ -25,6 +28,9 @@ namespace Exercicios
             {
 
                 case 1:
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine(" ____ ____ ____ ____ ____ \r\n||P |||a |||r |||e |||s ||\r\n||__|||__|||__|||__|||__||\r\n|/__\\|/__\\|/__\\|/__\\|/__\\|\n\n");
+                    Console.ResetColor();
 
                     Console.Write("Digite o número do início: ");
                     Console.ForegroundColor = ConsoleColor.DarkGreen;
@@ -65,6 +71,10 @@ namespace Exercicios
 
                 case 2:
 
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine(" ____ ____ ____ ____ ____ ____ ____ ____ ____ \r\n||F |||i |||b |||o |||n |||a |||c |||c |||i ||\r\n||__|||__|||__|||__|||__|||__|||__|||__|||__||\r\n|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|\n\n");
+                    Console.ResetColor();
+
                     Console.Write("Bem vindos a sequência de Fibonacci, digite quantas vezes deseja que a operação seja concluida: ");
                     Console.ForegroundColor = ConsoleColor.DarkGreen;
                     int quantidade = int.Parse(Console.ReadLine());
@@ -74,7 +84,7 @@ namespace Exercicios
                     double numeroAnterior = 0;
                     double fibonacci = 0;
 
-                    for (int i = 1; i <= quantidade; i++)
+                    for (int i = 0; i <= quantidade; i++)
                     {
 
                         fibonacci = numeroAtual + numeroAnterior;
@@ -89,7 +99,11 @@ namespace Exercicios
 
                     break;
 
-               case 3: //  a completar
+                case 3:
+
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine(" ____ ____ ____ ____ ____ ____ \r\n||P |||r |||i |||m |||o |||s ||\r\n||__|||__|||__|||__|||__|||__||\r\n|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|\n\n");
+                    Console.ResetColor();
 
                     Console.Write("Digite o número do início: ");
                     Console.ForegroundColor = ConsoleColor.DarkGreen;
@@ -102,9 +116,10 @@ namespace Exercicios
                     Console.ResetColor();
 
                     int contaPrimo = 0;
-                    int divisores = 0;
+
                     for (int i = inicio; i <= fim; i++)
                     {
+                        int divisores = 0;
 
                         if (i > 0)
                         {
@@ -113,9 +128,27 @@ namespace Exercicios
                                 if (i % j == 0)
                                 {
                                     divisores++;
+
                                 }
                             }
 
+                            if (divisores == 2)
+                            {
+
+                                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                                Console.Write(i);
+                                Console.ResetColor();
+                                Console.WriteLine(" É primo");
+                                contaPrimo++;
+
+                            }
+
+                            else
+                            {
+                                Console.WriteLine(i + " não é primo");
+                            }
+
+                            Console.WriteLine("Divisores: " + divisores + "\n");
                         }
 
                         else
@@ -125,17 +158,22 @@ namespace Exercicios
 
                         }
 
-
                     }
 
+                    Console.Write("\nHá um total de ");
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.Write(contaPrimo);
+                    Console.ResetColor();
+                    Console.WriteLine(" de números primos");
+
                     break;
+
+                default:
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Número invalido");
+                    break;
+
             }
-
-
-
-
-
-
 
 
             Console.ForegroundColor = ConsoleColor.DarkRed;
